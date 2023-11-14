0493ca68986e735a417521e105e91a0b2317e6e4
unknown <zainulabideen4578@gmail.com>
2023-11-14 19:02:45 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-2xl text-red-500'>Welcome to Tailwindcss</h1>
+    </div>
+  );
+}
+
+export default App;
